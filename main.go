09a7c063cbd55c2e546d1b691b82422510c0517b
Peeyush Singh <hello@pscop.in>
2019-03-27 13:14:32 +0530

    required args

--- main.go
+++ main.go
@@ -2,26 +2,30 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"log"
 	"io/ioutil"
-	"strings"
+	"log"
+	"os"
 	"os/exec"
+	"strings"
 )
 
-func main() {	
-	fpath := os.Args[1]
-	b, err:= ioutil.ReadFile(fpath)
-	if err!=nil {
-		log.Fatal(err)
-	}
-	funcs := strings.Split(string(b), "\n")
-	for i := 0; i < len(funcs) ; i++ {
-		// fmt.Println(funcs[i])
-		b, err := exec.Command("go", "get", "-u", funcs[i]).Output()
-		if err!=nil {
-			log.Fatal("error in sub", err)
+func main() {
+	if len(os.Args) == 2 {
+		fpath := os.Args[1]
+		b, err := ioutil.ReadFile(fpath)
+		if err != nil {
+			log.Fatal(err)
 		}
-		fmt.Println(string(b))
+		funcs := strings.Split(string(b), "\n")
+		for i := 0; i < len(funcs); i++ {
+			// fmt.Println(funcs[i])
+			b, err := exec.Command("go", "get", funcs[i]).Output()
+			if err != nil {
+				log.Fatal("error in sub", err)
+			}
+			fmt.Println(string(b))
+		}
+	} else {
+		fmt.Println("Insufficient Arguments")
 	}
-} 
+}
